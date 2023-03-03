7bdc0756eb328100146fb7b57575aa2676722dd5
BlackyFox <blacky@blackyfox.net>
2023-03-02 23:04:16 -0600

    CustomNames: Auto Name Duck WIP!! + New Ducks will be named whatever is on the clipboard * Open DuckRename Class for Modders
    TODO: Fix bug where Duck's are not being named

--- CustomNames/CustomNames.cs
+++ CustomNames/CustomNames.cs
@@ -5,6 +5,7 @@ using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Reflection.Emit;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using Object = UnityEngine.Object;
@@ -23,9 +24,10 @@ namespace Custom_Names
         private static GeneralManager _generalManager;
         private static readonly string _savePath = "./UserData/CustomNames.json";
         private static string _savecontent;
+        public static bool AutoName;
+        public static bool NewDuck = false;
         public static Dictionary<string, string> _duckNames = new();
         public static Dictionary<string, int> _ducks = new();
-        public static Action<string, string> _duckNameChanged;
 
         public override void OnEarlyInitializeMelon()
         {
@@ -34,6 +36,9 @@ namespace Custom_Names
 
         public override void OnInitializeMelon()
         {
+            CustomNameSettings.RegisterSettings();
+            AutoName = CustomNameSettings.AutoName.Value;
+
             //Save File
             if (!File.Exists(_savePath))
             {
@@ -54,6 +59,9 @@ namespace Custom_Names
 
                 harmony.PatchAll(typeof(GeneralManager_Start));
                 _instance.LoggerInstance.Msg("General Manager Add Patched!");
+
+                harmony.PatchAll(typeof(SpawnUpdate_Patch));
+                _instance.LoggerInstance.Msg("Spawn Update Patched!");
             }
             catch (Exception e)
             {
@@ -81,30 +89,37 @@ namespace Custom_Names
         {
             var intro = SceneManager.GetActiveScene().name == "Intro";
 
-            if (intro) return; // Check if Intro Scene is not loaded
-            if (Input.GetKeyDown(changename)) { SetName(); }
+            if (intro) { return; } // Check if Intro Scene is not loaded
+            if (Input.GetKeyDown(changename)) { DuckRename(); }
         }
 
-        private static void SetName()
+        public static void DuckRename(DuckManager duckManager = null, string duckName = null) //Void can be used for other mods
         {
-            var clipboard = GUIUtility.systemCopyBuffer;
-            if (clipboard == null) return;
-            if (_generalManager == null) { _instance.LoggerInstance.Error("Something went wrong"); return; }
-            var currentduck = _generalManager.Ducks[_generalManager.CurrentDuck].GetComponent<DuckManager>();
-            if (currentduck == null) { _instance.LoggerInstance.Error("Duck Not selected"); return; }
-            currentduck.NameChanged(currentduck.duckID, clipboard);
-            _instance.LoggerInstance.Msg("Duck: " + currentduck.duckID + " | Duck Name: " + clipboard);
-
-            Apply_Name(currentduck.duckID, clipboard);
+            if (duckName == null)
+            {
+                var clipboard = GUIUtility.systemCopyBuffer;
+                if (clipboard == null) { return; }
+                duckName = clipboard;
+            }
+            if (duckManager == null)
+            {
+                if (_generalManager == null) { _instance.LoggerInstance.Error("Something went wrong"); return; }
+                var currentduck = _generalManager.Ducks[_generalManager.CurrentDuck].GetComponent<DuckManager>();
+                if (currentduck == null) { _instance.LoggerInstance.Error("Duck Not selected"); return; }
+                duckManager = currentduck;
+            }
+            duckManager.NameChanged(duckManager.duckID, duckName);
+            _instance.LoggerInstance.Msg("Duck: " + duckManager.duckID + " | Duck Name: " + duckName);
+            Apply_Name(duckManager.duckID, duckName);
         }
 
-        public static string GetName(string DuckID)
+        public static string GetName(string DuckID) //Void can be used for other mods
         {
             if (_duckNames.TryGetValue(DuckID, out string duckname)) { return duckname; }
             return null;
         }
 
-        private static void Apply_Name(string duckID, string newduckName)
+        public static void Apply_Name(string duckID, string newduckName) //Void can be used for other mods
         {
             if (_duckNames.ContainsKey(duckID))
             {
@@ -114,7 +129,6 @@ namespace Custom_Names
             {
                 _duckNames.Add(duckID, newduckName);
             }
-            _duckNameChanged?.Invoke(duckID, newduckName);
             _savecontent = JsonConvert.SerializeObject(_duckNames, Formatting.Indented);
             File.WriteAllText(_savePath, _savecontent);
         }
@@ -123,7 +137,9 @@ namespace Custom_Names
         [HarmonyPatch(typeof(GeneralManager), "AddDuck")]
         public class GeneralManager_AddDuck
         {
-            static void Postfix(ref GeneralManager __instance ,DuckManager duckManager, string duckID, bool unlock = true, bool addToList = true) //TODO: Fix Add 2
+            //TODO: Fix Add 2 | Might be internal code \ Might be a bug in the game
+            //BUG: Duck Not Renaming while Spawning a New Duck
+            static void Postfix(ref GeneralManager __instance ,DuckManager duckManager, string duckID, bool unlock = true, bool addToList = true)
             {
                 int count = 0;
                 if (CustomNames._ducks.ContainsKey(duckID))
@@ -141,8 +157,17 @@ namespace Custom_Names
                     modifiedDuckID += "_" + count;
                 }
                 duckManager.duckID = modifiedDuckID;
-                var duckNames = CustomNames.GetName(modifiedDuckID);
-                duckManager.NameChanged(modifiedDuckID, duckNames);
+                if (CustomNames.NewDuck && CustomNames.AutoName)
+                {
+                    CustomNames._instance.LoggerInstance.Msg("New Duck!");
+                    CustomNames.NewDuck = false;
+                    CustomNames.DuckRename(duckManager);
+                }
+                else
+                {
+                    var duckNames = CustomNames.GetName(modifiedDuckID);
+                    duckManager.NameChanged(modifiedDuckID, duckNames);
+                }
             }
         }
         [HarmonyPatch(typeof(GeneralManager), "Start")]
@@ -152,6 +177,40 @@ namespace Custom_Names
             {
                 var duckNames = CustomNames.GetName("Duck1Base");
                 ___base1Duck.NameChanged("Duck1Base", duckNames);
+            }
+        }
+        [HarmonyPatch(typeof(GeneralManager), "SpawnUpdate")]
+        public class SpawnUpdate_Patch
+        {
+            static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator il)
+            {
+                var code = new List<CodeInstruction>(instructions);
+
+                int insertionIndex = -1;
+                Label returnDuck = il.DefineLabel();
+                for (int i = 0; i < code.Count - 1; i++)
+                {
+                    if (code[i].opcode == OpCodes.Stfld && code[i+1].opcode == OpCodes.Ldarg_0)
+                    {
+                        insertionIndex = i;
+                        code[i].labels.Add(returnDuck);
+                        break;
+                    }
+                }
+
+                var instructionsToInsert = new List<CodeInstruction>();
+                //
+                // CustomNames.NewDuck = True
+                //
+                instructionsToInsert.Add(new CodeInstruction(OpCodes.Ldc_I4_1, (sbyte)4));
+                instructionsToInsert.Add(new CodeInstruction(OpCodes.Stsfld, AccessTools.Field(typeof(CustomNames), "NewDuck")));
+
+                if (insertionIndex != -1)
+                {
+                    code.InsertRange(insertionIndex, instructionsToInsert);
+                }
+
+                return code;
             }
         }
     }
---
+++ CustomNames/CustomNamesSettings.cs
@@ -0,0 +1,19 @@
+﻿using MelonLoader;
+
+#nullable disable
+
+namespace Custom_Names
+{
+    public static class CustomNameSettings //MelonSettings for the Mod
+    {
+        private const string SettingsCategory = "CustomNames";
+        internal static MelonPreferences_Entry<bool> AutoName;
+
+        internal static void RegisterSettings()
+        {
+            var category = MelonPreferences.CreateCategory(SettingsCategory, "CustomNames");
+            AutoName = category.CreateEntry("AutoName", false, "Auto Name",
+                "Newly Spawning Ducks will be Auto Nammed via clipboard");
+        }
+    }
+}
