94fb3ca0634daaf63d6b601a719e0497cf947b56
BlackyFox <blacky@blackyfox.net>
2024-04-24 21:09:23 -0500

    Simplified Code base for EventRennabler

--- EventReenable/EventReenable.cs
+++ EventReenable/EventReenable.cs
@@ -1,38 +1,15 @@
-using Event_Reenable;
 using HarmonyLib;
 using MelonLoader;
-using System;
-using System.Collections.Generic;
-using UnityEngine.AddressableAssets;
 
 [assembly: MelonInfo(typeof(EventReenable), "Event Reenabler", "0.1.1", "BlackyFox", "https://github.com/KitsueFox/PPDS-Mods")]
 [assembly: MelonGame("Turbolento Games", "Placid Plastic Duck Simulator")]
 
-namespace Event_Reenable
-{
-
     public class EventReenable : MelonMod
     {
-        internal static EventReenable _instance { get; set; }
-
-        public override void OnEarlyInitializeMelon()
-        {
-            _instance = this;
-        }
-
         public override void OnInitializeMelon()
         {
             var harmony = new HarmonyLib.Harmony("Event_Reenable");
-            try
-            {
                 harmony.PatchAll(typeof(SeasonalPatch_DLCManager));
-                _instance.LoggerInstance.Msg("DLCManager Patched!");
-            }
-            catch (Exception e)
-            {
-
-                _instance.LoggerInstance.Msg(e);
-            }
         }
     }
 
@@ -40,11 +17,10 @@ namespace Event_Reenable
     [HarmonyPatch(typeof(DLCManager), "SeasonalSetSetup")]
     public class SeasonalPatch_DLCManager
     {
-        static bool Prefix(ref DLCManager __instance, SetToggle ___seasonalSetToggle)
+        static bool Prefix(SetToggle ___seasonalSetToggle)
         {
             ___seasonalSetToggle.gameObject.SetActive(true);
             ___seasonalSetToggle.Setup(true);
             return false;
         }
     }
-}
